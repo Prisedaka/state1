979a0797ddcb527196299c2f598d624f70f416a2
prisedaka <prisedaka@ya.ru>
2019-01-03 21:34:51 +0300

    q

--- src/AlarmClock.js
+++ src/AlarmClock.js
@@ -1,11 +1,28 @@
 import State from './State';
+// import ClockState from './ClockState';
 
 export default class {
   constructor() {
-    this.minutes = 0;
+    this.min = 0;
+    this.hrs = 12;
+    this.alarmMin = 0;
+    this.alarmHrs = 6;
+    this.setState(ClockState);
+  }
+  setState(Klass) {
+    this.setState = new Klass(this);
   }
   minutes() {
-    console.log(this.minutes);
+    return this.min;
+  }
+  hours() {
+    return this.hrs;
+  }
+  alarmHours() {
+    return this.alarmHrs;
+  }
+  alarmMinutes() {
+    return this.alarmMin;
   }
 }
 
